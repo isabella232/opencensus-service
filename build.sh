663fe763ca3d84949a063d364809b9a571b8d7b2
Ian Quick <ian.quick@gmail.com>
2019-05-03 10:10:36 -0400

    fscking strings

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -e
 DOCKER_TAG=${1:-$PIPA_IMAGE_FULL_NAME}
-
 if [[ -z "${DOCKER_TAG}" ]]; then
     echo "$0 [image]"
     exit 1
 fi
 
-docker build -f ./cmd/occollector/Dockerfile.shopify -t $DOCKER_TAG .
+pipa build -x --push -- docker build -f ./cmd/occollector/Dockerfile.shopify -t $DOCKER_TAG .
+pipa vouch
